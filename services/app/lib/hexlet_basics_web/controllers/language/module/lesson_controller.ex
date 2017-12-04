89114b2f91ab0dc2fd857b681f3111f5bc39fe78
Rakhim Davletkaliyev <freetonik@gmail.com>
2017-12-04 18:41:42 +0200

    lesson progress counter

--- services/app/lib/hexlet_basics_web/controllers/language/module/lesson_controller.ex
+++ services/app/lib/hexlet_basics_web/controllers/language/module/lesson_controller.ex
@@ -3,6 +3,7 @@ defmodule HexletBasicsWeb.Language.Module.LessonController do
   alias HexletBasics.Repo, as: Repo
   alias HexletBasics.Language, as: Language
   import PhoenixGon.Controller
+  import Ecto.Query
 
   plug :put_layout, "lesson.html"
 
@@ -14,8 +15,19 @@ defmodule HexletBasicsWeb.Language.Module.LessonController do
     language = Repo.get_by!(Language, slug: language_id)
     module = Repo.get_by!(Language.Module, language_id: language.id, slug: module_id)
     module_description = Repo.get_by!(Language.Module.Description,  module_id: module.id, locale: "ru")
+
+    query = from l in Language.Module.Lesson,
+      where: l.language_id == ^language.id and l.upload_id == ^language.upload_id and l.module_id == ^module.id,
+      order_by: [asc: l.order]
+    lessons = Repo.all(query)
+    module_length = length(lessons)
+
+    
+    # test = testLesson.order
+
     lesson = Repo.get_by!(Language.Module.Lesson,  language_id: language.id, module_id: module.id, slug: id)
     lesson_description = Repo.get_by!(Language.Module.Lesson.Description,  lesson_id: lesson.id, locale: "ru")
+    lesson_order_natural = Enum.find_index(lessons, fn(x) -> x.order == lesson.order end) + 1
 
     conn = put_gon(conn, lesson: lesson, lesson_description: lesson_description, language: language)
 
@@ -24,7 +36,9 @@ defmodule HexletBasicsWeb.Language.Module.LessonController do
       module: module,
       lesson: lesson,
       module_description: module_description,
-      lesson_description: lesson_description
+      lesson_description: lesson_description,
+      module_length: module_length,
+      lesson_order_natural: lesson_order_natural
   end
 end
 
