3fecddcabdb88b67fc0769143b34f5797f19c34c
Kirill Mokevnin <mokevnin@gmail.com>
2017-12-09 23:54:42 +0100

    add code directory to dev configuration

--- services/app/config/dev.exs
+++ services/app/config/dev.exs
@@ -46,6 +46,9 @@ config :hexlet_basics, HexletBasicsWeb.Endpoint,
     ]
   ]
 
+config :hexlet_basics,
+  code_directory: "/tmp/hexlet-basics/code"
+
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
