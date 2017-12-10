eebeb85ebaafa3c92f5ead87e99a7ab3f7af6ef5
Kirill Mokevnin <mokevnin@gmail.com>
2017-12-10 12:59:50 +0100

    add docker role from ansible galaxy

--- make-ansible.mk
+++ make-ansible.mk
@@ -1,3 +1,5 @@
+ansible-deps-install:
+	ansible-galaxy install -r requirements.yml
 ansible-vaults-encrypt:
 	ansible-vault encrypt ansible/production/group_vars/all/vault.yml
 	ansible-vault encrypt ansible/development/group_vars/all/vault.yml
