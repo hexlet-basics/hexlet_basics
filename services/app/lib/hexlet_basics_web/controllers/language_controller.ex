1935cd6fc83a1d9f2da01c089bff11df5c5328f4
Kirill Mokevnin <mokevnin@gmail.com>
2017-12-18 00:38:00 +0100

    fix language controller

--- services/app/lib/hexlet_basics_web/controllers/language_controller.ex
+++ services/app/lib/hexlet_basics_web/controllers/language_controller.ex
@@ -6,7 +6,7 @@ defmodule HexletBasicsWeb.LanguageController do
   import Ecto.Query
 
   def show(conn, %{"id" => id}) do
-    language = Repo.get_by(HexletBasics.Language, slug: id)
+    language = Repo.get_by!(HexletBasics.Language, slug: id)
     query = from m in Language.Module,
       where: m.language_id == ^language.id and m.upload_id == ^language.upload_id,
       order_by: [asc: m.order]
