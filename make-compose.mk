5771e9b0f15622671a971965606c6417303f0a92
Kirill Mokevnin <mokevnin@gmail.com>
2017-12-28 16:16:09 +0100

    fix make-compose.mk

--- make-compose.mk
+++ make-compose.mk
@@ -34,4 +34,4 @@ compose-exercises-load:
 	docker pull hexlet/hexlet-basics-exercises-php
 	rm -rf tmp/exercises-php
 	docker run --rm -v $(CURDIR)/tmp/exercises-php:/out hexlet/hexlet-basics-exercises-php bash -c "cp -r /exercises-php/* /out"
-	docker-compose run --rm --no-deps -v $(CURDIR)/tmp/exercises-php:/exercises-php app mix x.exercises.load php
+	docker-compose run --rm -v $(CURDIR)/tmp/exercises-php:/exercises-php app mix x.exercises.load php
