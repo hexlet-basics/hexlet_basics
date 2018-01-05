0e2325ad85561a9d9d07bfd2e8eaacdebf54702c
Kirill Mokevnin <mokevnin@gmail.com>
2018-01-05 21:15:39 +0300

    remove vault password

--- make-ansible.mk
+++ make-ansible.mk
@@ -21,11 +21,11 @@ ansible-site-deploy:
 
 ansible-vaults-encrypt:
 	ansible-vault encrypt ansible/production/group_vars/all/vault.yml
-	ansible-vault encrypt ansible/development/group_vars/all/vault.yml
+	# ansible-vault encrypt ansible/development/group_vars/all/vault.yml
 
 ansible-vaults-decrypt:
 	ansible-vault decrypt ansible/production/group_vars/all/vault.yml
-	ansible-vault decrypt ansible/development/group_vars/all/vault.yml
+	# ansible-vault decrypt ansible/development/group_vars/all/vault.yml
 
 ansible-development-setup:
 	mkdir -p tmp
