854c48cb9230834ef3d187a8a04027bb0b8d8836
Kirill Mokevnin <mokevnin@gmail.com>
2018-01-24 10:36:22 +0700

    add dialyxir

--- mix.exs
+++ mix.exs
@@ -4,12 +4,16 @@ defmodule Mixfile do
   def project do
     [
       app: :hexlet_basics,
+      version: "0.0.1",
+      elixir: "~> 1.6",
       deps: deps()
     ]
   end
+
   defp deps do
     [
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
--- services/app/mix.exs
+++ services/app/mix.exs
@@ -5,7 +5,7 @@ defmodule HexletBasics.Mixfile do
     [
       app: :hexlet_basics,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers, # ++ [:jsroutes],
       start_permanent: Mix.env == :prod,
