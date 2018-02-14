fe0cabcfcf2ad67d532f608d744782a232d778b8
Kirill Mokevnin <mokevnin@gmail.com>
2018-02-14 21:56:05 +0800

    fix make

--- make-production.mk
+++ make-production.mk
@@ -5,7 +5,7 @@ production-exercises-load:
 	ansible-playbook ansible/exercises.yml -i ansible/production -u $U -vv --ask-vault-pass
 
 production-setup:
-	ansible-playbook ansible/site.yml -i ansible/production -u $U
+	ansible-playbook ansible/site.yml -i ansible/production -u $U --ask-vault-pass
 
 production-env-update:
 	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --tag env
