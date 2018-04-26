18bb137bb0b14f08f7b9e8e25712281870fb44e8
Kirill Mokevnin <mokevnin@gmail.com>
2018-04-27 01:43:56 +0300

    change server

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,17 +1,20 @@
-resource "aws_instance" "hexlet-basics-web" {
+resource "aws_instance" "hexlet-basics-web2" {
   ami           = "ami-e22aaa8d"
-  instance_type = "t2.small"
-  subnet_id = "${aws_subnet.hexlet-basics-app-a.id}"
+  instance_type = "t2.medium"
+  subnet_id = "${aws_subnet.hexlet-basics-app-b.id}"
   associate_public_ip_address = true
   key_name = "${aws_key_pair.kirillm.id}"
   iam_instance_profile = "${aws_iam_instance_profile.webserver.name}"
+  root_block_device = {
+    volume_size = 20
+  }
   vpc_security_group_ids = [
     "${aws_security_group.hexlet-basics-ssh.id}",
     "${aws_security_group.hexlet-basics-http.id}"
   ]
 
   tags {
-    Name = "hexlet-basics-web-a"
+    Name = "hexlet-basics-web"
     Project = "hexlet-basics"
   }
 }
@@ -37,7 +40,7 @@ resource "aws_lb_target_group" "hexlet-basics" {
 
 resource "aws_lb_target_group_attachment" "hexlet-basics" {
   target_group_arn = "${aws_lb_target_group.hexlet-basics.arn}"
-  target_id        = "${aws_instance.hexlet-basics-web.id}"
+  target_id        = "${aws_instance.hexlet-basics-web2.id}"
   port             = 80
 }
 
