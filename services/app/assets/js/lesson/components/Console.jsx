2abcfaa675885aa3ff9ad8924d0f7f056c3afda9
Kirill Mokevnin <mokevnin@gmail.com>
2018-06-26 17:36:20 +0400

    fix sanitizing

--- services/app/assets/js/lesson/components/Console.jsx
+++ services/app/assets/js/lesson/components/Console.jsx
@@ -3,7 +3,10 @@ import sanitizeHtml from 'sanitize-html';
 import ansiUp from '../../lib/ansi_up';
 
 const Console = ({ output, className }) => {
-  const html = ansiUp(sanitizeHtml(output));
+  const sanitizedOutput = sanitizeHtml(output, {
+    textFilter: text => text.replace(/&quot;/g, '\''),
+  });
+  const html = ansiUp(sanitizedOutput);
   return (
     <div className={className}>
       <pre>
