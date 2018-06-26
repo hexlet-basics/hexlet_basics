62194b725e85f1e2e0cbb4eb58a7baf99fa09313
Kirill Mokevnin <mokevnin@gmail.com>
2018-06-26 15:05:13 +0400

    update deps

--- services/app/mix.exs
+++ services/app/mix.exs
@@ -42,7 +42,7 @@ defmodule HexletBasics.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.3.0"},
-      {:plug, "1.5.0"}, # remove after upgrade phoenix > 1.3.2
+      {:plug, "~> 1.5.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
