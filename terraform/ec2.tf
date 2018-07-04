686200dc285df42da571fce520a06fe98beb947a
Vagin Alexander <laplugin73@gmail.com>
2018-07-04 08:39:54 +0000

    add cert to lb listener

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -64,18 +64,19 @@ resource "aws_lb_listener" "hexlet-basics" {
   }
 }
 
+resource "aws_lb_listener_certificate" "hexlet-basics-listener-cert" {
+  listener_arn    = "${aws_lb_listener.hexlet-basics-443.arn}"
+  certificate_arn = "${aws_acm_certificate.hexlet-basics-lb-cert.arn}"
+}
+
 resource "aws_lb_listener" "hexlet-basics-443" {
   load_balancer_arn = "${aws_lb.hexlet-basics.arn}"
   port              = "443"
   protocol          = "HTTPS"
+  certificate_arn = "${aws_acm_certificate.hexlet-basics-lb-cert.arn}"
 
   default_action {
     target_group_arn = "${aws_lb_target_group.hexlet-basics.arn}"
     type             = "forward"
   }
-}
-
-resource "aws_lb_listener_certificate" "hexlet-basics-listener-cert" {
-  listener_arn    = "${aws_lb_listener.hexlet-basics-443.arn}"
-  certificate_arn = "${aws_acm_certificate.hexlet-basics-lb-cert.arn}"
 }
