fa11defb1dba87c3e732bd622a42364c82082ee7
Vagin Alexander <laplugin73@gmail.com>
2018-07-04 12:17:59 +0000

    fix ports range

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -116,7 +116,7 @@ resource "aws_security_group" "hexlet-basics-https" {
 
   ingress {
     from_port   = 443
-    to_port     = 80
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
