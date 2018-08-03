4082255a835972f9fa4c90b062b1696d89833df8
Kirill Mokevnin <mokevnin@gmail.com>
2018-08-03 22:10:47 +0400

    add build trigger for nginx

--- terraform-gcp/cloudbuild.tf
+++ terraform-gcp/cloudbuild.tf
@@ -8,3 +8,14 @@ resource "google_cloudbuild_trigger" "app" {
   }
   filename = "services/app/cloudbuild.yaml"
 }
+
+resource "google_cloudbuild_trigger" "nginx" {
+  project  = "${var.project_name}"
+  description = "nginx"
+  trigger_template {
+    branch_name = "master"
+    project     = "${var.project_name}"
+    repo_name   = "${var.repository}"
+  }
+  filename = "services/nginx/cloudbuild.yaml"
+}
