c257d9a09982a7f536f98cf8bc1c651f2542e053
Vagin Alexander <laplugin73@gmail.com>
2018-09-09 11:41:36 +0000

    wuild with tags

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -15,6 +15,23 @@ resource "google_cloudbuild_trigger" "app" {
   }
 }
 
+resource "google_cloudbuild_trigger" "app-tag" {
+  project     = "${var.project_name}"
+  description = "build with tag"
+
+  trigger_template {
+    tag_name  = "v\\d.*"
+    project     = "${var.project_name}"
+    repo_name   = "${google_sourcerepo_repository.hexlet_basics.name}"
+  }
+
+  filename = "services/app/cloudbuild.yaml"
+
+  substitutions = {
+    _SLACK_WEBHOOK = "${var.slack_codebuild_webhook}"
+  }
+}
+
 resource "google_cloudbuild_trigger" "nginx" {
   project     = "${var.project_name}"
   description = "nginx"
