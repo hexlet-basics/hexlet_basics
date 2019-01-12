ec5821c41b384f8d10fc753131702217abb78f38
Kirill Mokevnin <mokevnin@gmail.com>
2019-01-12 21:00:44 +0400

    fix dev.exs

--- services/app/config/dev.exs
+++ services/app/config/dev.exs
@@ -56,7 +56,6 @@ config :hexlet_basics, HexletBasicsWeb.Endpoint,
 config :hexlet_basics,
   code_directory: "/tmp/hexlet-basics/code",
   langs: %{"ru.hexlet-basics.test" => "ru", "en.hexlet-basics.test" => "en"},
-  docker_command_template: "docker run ~s ~s timeout -t 10 -s KILL make --silent -C ~s test",
   ga_ru: "ruGtag",
   ga_en: "enGtag"
 
