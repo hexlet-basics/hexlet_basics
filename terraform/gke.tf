901f58ed302e345e7654edb5000069a1ce82c8dd
Vagin <laplugin73@gmail.com>
2019-06-04 08:55:02 +0000

    add new kub cluster

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -33,3 +33,39 @@ resource "google_container_cluster" "hexlet_basics" {
     command = "gcloud container clusters get-credentials --region ${var.zone} ${var.gke_cluster_name}"
   }
 }
+
+resource "google_container_cluster" "hexlet_basics_2" {
+  min_master_version = "1.12.7-gke.10"
+
+  # remove_default_node_pool = true
+  project            = "${var.project_name}"
+  name               = "${var.gke_cluster_name_2}"
+  zone               = "${var.zone}"
+  initial_node_count = 1
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+
+    machine_type = "n1-standard-1"
+    disk_size_gb = 50
+    disk_type    = "pd-ssd"
+
+    # management {
+    #   auto_repair  = true
+    #   auto_upgrade = false
+    # }
+
+  }
+
+
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials --region ${var.zone} ${var.gke_cluster_name_2}"
+  }
+}
