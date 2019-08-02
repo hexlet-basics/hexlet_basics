10d264615970987e000a6d4105b125c6da799548
n.mikhaylov <nmikhaylov92@gmail.com>
2019-08-02 16:48:02 +0300

    add text-black class

--- services/app/assets/js/lesson/components/HTMLPreview.jsx
+++ services/app/assets/js/lesson/components/HTMLPreview.jsx
@@ -6,7 +6,7 @@ const HTMLPreview = (props) => {
   const { html } = props;
   return (
     <div className="mt-2 p-2 h-50 overflow-auto bg-light">
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <div className="text-black" dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
 };
