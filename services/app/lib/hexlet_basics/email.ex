8e2ebffe5660d8cb2e21eb56b2cd9867084fa62e
Nikita Naumenko <naumenkoniki@gmail.com>
2019-08-15 07:05:37 +0000

    Fix email templates and add humanize from sender

--- services/app/lib/hexlet_basics/email.ex
+++ services/app/lib/hexlet_basics/email.ex
@@ -3,16 +3,21 @@ defmodule HexletBasics.Email do
   import HexletBasicsWeb.Gettext
   import HexletBasicsWeb.Helpers.CustomUrl
 
+  def base_email(email_address, conn, subject) do
+    new_email()
+    |> to(email_address)
+    |> from({"Code Basics", sending_from(conn) })
+    |> subject(subject)
+    |> put_html_layout({HexletBasicsWeb.LayoutView, "email.html"})
+  end
+
   def confirmation_html_email(conn, email_address, path) do
     subject = gettext("Confirm registration")
     %{assigns: %{locale: locale}} = conn
     url = build_url(conn, path)
 
-    new_email()
-    |> to(email_address)
-    |> from(sending_from(conn))
-    |> subject(subject)
-    |> put_html_layout({HexletBasicsWeb.LayoutView, "email.html"})
+    email_address
+    |>base_email(conn, subject)
     |> render(
       "user/confirmation.#{locale}.html",
       confirmation_url: url,
@@ -24,11 +29,8 @@ defmodule HexletBasics.Email do
     url = build_url(conn, path)
     %{assigns: %{locale: locale}} = conn
 
-    new_email()
-    |> to(email_address)
-    |> from(sending_from(conn))
-    |> subject(subject)
-    |> put_html_layout({HexletBasicsWeb.LayoutView, "email.html"})
+    email_address
+    |>base_email(conn, subject)
     |> render(
       "user/reset_password.#{locale}.html",
       reset_password_url: url,
