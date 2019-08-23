8fea5ca79ace3b1bbba4c00dd09c8e3271d24e6d
Nikita Naumenko <naumenkoniki@gmail.com>
2019-08-23 14:43:52 +0300

    Add lessons index action (#82)
    * add lessons index action
    
    * fix locale
    
    * Add whitespice

--- services/app/lib/hexlet_basics_web/controllers/language/module/lesson_controller.ex
+++ services/app/lib/hexlet_basics_web/controllers/language/module/lesson_controller.ex
@@ -7,11 +7,51 @@ defmodule HexletBasicsWeb.Language.Module.LessonController do
   alias HexletBasics.Language.Module.Lesson
   import PhoenixGon.Controller
   import Ecto.Query
-  # require Logger
 
-  # plug HexletBasicsWeb.Plugs.RequireAuth
+  def index(conn, %{"module_id" => module_id, "language_id" => language_id}) do
+    %{assigns: %{current_user: current_user, locale: locale}} = conn
 
-  plug(:put_layout, "lesson.html")
+    language = Repo.get_by(Language, slug: language_id)
+    module = Repo.get_by(Language.Module, language_id: language.id, slug: module_id)
+
+    module_description_assoc = Ecto.assoc(module, :descriptions)
+    module_description_query =
+      from(d in module_description_assoc,
+        where: d.locale == ^locale
+      )
+    module_description = Repo.one(module_description_query)
+
+    user_finished_lessons_by_lesson =
+      current_user
+      |> Ecto.assoc(:finished_lessons)
+      |> Repo.all()
+      |> ExtEnum.key_by(:language_module_lesson_id)
+
+    lesson_descriptions_assoc = Ecto.assoc(language, :lesson_descriptions)
+    lesson_description_query =
+      from(d in lesson_descriptions_assoc,
+        where: d.locale == ^locale
+      )
+    descriptions_by_lesson =
+      lesson_description_query
+      |> Repo.all()
+      |> ExtEnum.key_by(:lesson_id)
+
+    query = from l in Language.Module.Lesson,
+      where: l.language_id == ^language.id and l.upload_id == ^language.upload_id and l.module_id == ^module.id,
+      order_by: [asc: l.order]
+    lessons = Repo.all(query)
+
+    render(
+      conn,
+      language: language,
+      module: module,
+      lessons: lessons,
+      user_finished_lessons_by_lesson: user_finished_lessons_by_lesson,
+      descriptions_by_lesson: descriptions_by_lesson,
+      module_description: module_description
+    )
+  end
 
   def show(conn, %{"id" => id, "module_id" => module_id, "language_id" => language_id}) do
     %{assigns: %{locale: locale, current_user: current_user}} = conn
@@ -71,8 +111,6 @@ defmodule HexletBasicsWeb.Language.Module.LessonController do
           |> Serializers.PrevLessonSerializer.to_map()
       end
 
-    # Logger.debug(inspect(prev_lesson.module))
-
     conn =
       put_gon(conn,
         lesson: lesson,
@@ -95,7 +133,8 @@ defmodule HexletBasicsWeb.Language.Module.LessonController do
       lesson_instructions_html: lesson_instructions_html,
       module_description: module_description,
       lesson_description: lesson_description,
-      lessons_count: lessons_count
+      lessons_count: lessons_count,
+      layout: {HexletBasicsWeb.LayoutView, "lesson.html"}
     )
   end
 end
