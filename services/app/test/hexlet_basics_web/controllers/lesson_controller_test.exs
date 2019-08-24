f4ebb12d444a3cb019c5923f4e8d3643d58ac7ec
Nikita Naumenko <naumenkoniki@gmail.com>
2019-08-24 10:51:35 +0000

    Rename variable

--- services/app/test/hexlet_basics_web/controllers/lesson_controller_test.exs
+++ services/app/test/hexlet_basics_web/controllers/lesson_controller_test.exs
@@ -33,7 +33,7 @@ defmodule HexletBasicsWeb.LessonControllerTest do
       })
     user_with_finished_lessons = insert(:user)
     user_finished_lesson = insert(:user_finished_lesson, %{language_module_lesson: finished_lesson, user: user_with_finished_lessons})
-    user_finished_lesson = insert(:user_finished_lesson, %{language_module_lesson: not_finished_lesson, user: user_with_finished_lessons})
+    second_user_finished_lesson = insert(:user_finished_lesson, %{language_module_lesson: not_finished_lesson, user: user_with_finished_lessons})
     user = insert(:user)
 
     conn =  conn
