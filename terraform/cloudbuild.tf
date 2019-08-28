5ca868c101d4062b1b33a662fed069f63991acc8
Nikita Naumenko <naumenkoniki@gmail.com>
2019-08-28 06:37:19 +0000

    add caddy to cloudbuild

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -47,6 +47,23 @@ resource "google_cloudbuild_trigger" "nginx" {
   filename = "services/nginx/cloudbuild.yaml"
 }
 
+resource "google_cloudbuild_trigger" "caddy" {
+  project     = "${var.project_name}"
+  description = "caddy"
+
+  trigger_template {
+    tag_name  = "v\\d.*"
+    # project   = "${var.project_name}"
+    repo_name = "${var.repositories["hexlet_basics"]}"
+  }
+
+  substitutions = {
+    _SLACK_WEBHOOK = "${var.slack_codebuild_webhook}"
+  }
+
+  filename = "services/caddy/cloudbuild.yaml"
+}
+
 # resource "google_cloudbuild_trigger" "exercises_php" {
 #   project     = "${var.project_name}"
 #   description = "exercises_php"
