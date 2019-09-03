6dabc4298a59574e9a050c1ab9a4c6ecfa2e304a
Nikita Naumenko <naumenkoniki@gmail.com>
2019-09-03 07:27:15 +0000

    delete nginx

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -30,23 +30,6 @@ resource "google_cloudbuild_trigger" "app-tag" {
   }
 }
 
-resource "google_cloudbuild_trigger" "nginx" {
-  project     = "${var.project_name}"
-  description = "nginx"
-
-  trigger_template {
-    tag_name  = "v\\d.*"
-    # project   = "${var.project_name}"
-    repo_name = "${var.repositories["hexlet_basics"]}"
-  }
-
-  substitutions = {
-    _SLACK_WEBHOOK = "${var.slack_codebuild_webhook}"
-  }
-
-  filename = "services/nginx/cloudbuild.yaml"
-}
-
 resource "google_cloudbuild_trigger" "caddy" {
   project     = "${var.project_name}"
   description = "caddy"
