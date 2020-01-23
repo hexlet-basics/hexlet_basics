28e8e5f2522b93e7232134790e6ca3efa7eb0347
n.mikhaylov <nmikhaylov92@gmail.com>
2020-01-23 17:19:27 +0300

    fix HTMLPreview block

--- services/app/assets/js/lesson/components/HTMLPreview.jsx
+++ services/app/assets/js/lesson/components/HTMLPreview.jsx
@@ -6,7 +6,7 @@ import Frame from 'react-frame-component';
 const HTMLPreview = (props) => {
   const { html } = props;
   return (
-    <div className="mt-2 p-2 h-50 overflow-auto bg-light">
+    <div className="mt-2 p-2 h-50 bg-white">
       <Frame className="border-0 h-100 w-100">
         <div className="text-black" dangerouslySetInnerHTML={{ __html: html }} />
       </Frame>
