bf161b8761bd9f9d0651d53cfa35f4dcb315ec70
Nikita Naumenko <naumenkoniki@gmail.com>
2020-01-23 12:08:17 +0000

    fix duplicate title

--- services/app/lib/hexlet_basics_web/controllers/user_controller.ex
+++ services/app/lib/hexlet_basics_web/controllers/user_controller.ex
@@ -10,7 +10,23 @@ defmodule HexletBasicsWeb.UserController do
 
   def new(conn, _params) do
     changeset = User.registration_changeset(%User{}, %{})
-    render(conn, "new.html", changeset: changeset)
+    meta_attrs = [
+      %{property: "og:type", content: 'website'},
+      %{property: "og:title", content: gettext("Code Basics Registration Title")},
+      %{property: "og:description", content: gettext("Code Basics Registration Description")},
+      %{property: "og:image", content: Routes.static_url(conn, "/images/logo.png")},
+      %{property: "og:url", content: Routes.page_url(conn, :index)},
+      %{property: "description", content: gettext("Code Basics Registration Description")},
+      %{property: "image", content: Routes.static_url(conn, "/images/logo.png")}
+    ]
+    link_attrs = [
+      %{rel: "canonical", href: Routes.page_url(conn, :index)},
+      %{rel: 'image_src', href: Routes.static_url(conn, "/images/logo.png")}
+    ]
+
+    title_text = gettext("Code Basics Registration Title")
+
+    render(conn, "new.html", changeset: changeset, meta_attrs: meta_attrs, link_attrs: link_attrs, title: title_text)
   end
 
   def create(conn, %{"user" => params}) do
