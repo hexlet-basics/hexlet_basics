f40f1041972cbbd1d0c08b403f66946a9b0b3cc7
Nikita Naumenko <naumenkoniki@gmail.com>
2020-01-24 09:36:19 +0300

    Fix seo (#150)
    * fix titles
    
    * fix duplicate title
    
    * fix h1 warning

--- services/app/lib/hexlet_basics_web/controllers/remind_password_controller.ex
+++ services/app/lib/hexlet_basics_web/controllers/remind_password_controller.ex
@@ -5,7 +5,23 @@ defmodule HexletBasicsWeb.RemindPasswordController do
 
   def new(conn, _params) do
     changeset = User.changeset(%User{}, %{})
-    render(conn, "new.html", changeset: changeset)
+    meta_attrs = [
+      %{property: "og:type", content: 'website'},
+      %{property: "og:title", content: gettext("Code Basics Remind Password Title")},
+      %{property: "og:description", content: gettext("Code Basics Remind Password Description")},
+      %{property: "og:image", content: Routes.static_url(conn, "/images/logo.png")},
+      %{property: "og:url", content: Routes.page_url(conn, :index)},
+      %{property: "description", content: gettext("Code Basics Remind Password Description")},
+      %{property: "image", content: Routes.static_url(conn, "/images/logo.png")}
+    ]
+    link_attrs = [
+      %{rel: "canonical", href: Routes.page_url(conn, :index)},
+      %{rel: 'image_src', href: Routes.static_url(conn, "/images/logo.png")}
+    ]
+
+    title_text = gettext("Code Basics Remind Password Title")
+
+    render(conn, "new.html", changeset: changeset, meta_attrs: meta_attrs, link_attrs: link_attrs, title: title_text)
   end
 
   def create(conn, %{"user" => params}) do
--- services/app/lib/hexlet_basics_web/controllers/session_controller.ex
+++ services/app/lib/hexlet_basics_web/controllers/session_controller.ex
@@ -8,7 +8,23 @@ defmodule HexletBasicsWeb.SessionController do
   plug CheckAuthentication when action in [:new, :create]
 
   def new(conn, _params) do
-    render(conn, "new.html")
+    meta_attrs = [
+      %{property: "og:type", content: 'website'},
+      %{property: "og:title", content: gettext("Code Basics Login Title")},
+      %{property: "og:description", content: gettext("Code Basics Login Description")},
+      %{property: "og:image", content: Routes.static_url(conn, "/images/logo.png")},
+      %{property: "og:url", content: Routes.page_url(conn, :index)},
+      %{property: "description", content: gettext("Code Basics Login Description")},
+      %{property: "image", content: Routes.static_url(conn, "/images/logo.png")}
+    ]
+    link_attrs = [
+      %{rel: "canonical", href: Routes.page_url(conn, :index)},
+      %{rel: 'image_src', href: Routes.static_url(conn, "/images/logo.png")}
+    ]
+
+    title_text = gettext("Code Basics Login Title")
+
+    render(conn, "new.html", link_attrs: link_attrs, meta_attrs: meta_attrs, title: title_text)
   end
 
   def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
