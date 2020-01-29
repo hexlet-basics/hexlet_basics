152398f5bb898e4923e6d34093d99d3ad3fb7a95
Nikita Naumenko <naumenkoniki@gmail.com>
2020-01-29 15:38:43 +0300

    fix module page for english locale (#160)

--- services/web/lib/hexlet_basics_web/views/language/module_view.ex
+++ services/web/lib/hexlet_basics_web/views/language/module_view.ex
@@ -1,4 +1,6 @@
 defmodule HexletBasicsWeb.Language.ModuleView do
   use HexletBasicsWeb, :view
+
+  def locales_switch_map, do: %{"ru" => "en", "en" => "ru"}
 end
 
--- services/web/lib/hexlet_basics_web/views/language_view.ex
+++ services/web/lib/hexlet_basics_web/views/language_view.ex
@@ -1,3 +1,5 @@
 defmodule HexletBasicsWeb.LanguageView do
   use HexletBasicsWeb, :view
+
+  def locales_switch_map, do: %{"ru" => "en", "en" => "ru"}
 end
