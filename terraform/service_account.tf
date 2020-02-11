fdd2e6aa9f8fd32e00da99d073004dc8db9aab9f
plugin73 <laplugin73@gmail.com>
2020-02-11 05:54:40 +0000

    rewrite terraform configs to new rules

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -1,14 +1,14 @@
 resource "google_service_account" "cloudsql_proxy" {
-  project    = "${var.project_name}"
+  project    = var.project_name
   account_id = "cloudsql-proxy"
 }
 
 resource "google_project_iam_member" "cloudsql_proxy" {
-  project = "${var.project_name}"
+  project = var.project_name
   role    = "roles/cloudsql.client"
   member  = "serviceAccount:${google_service_account.cloudsql_proxy.email}"
 }
 
 resource "google_service_account_key" "cloudsql_proxy" {
-  service_account_id = "${google_service_account.cloudsql_proxy.name}"
+  service_account_id = google_service_account.cloudsql_proxy.name
 }
