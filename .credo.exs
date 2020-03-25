50505ccb1794bc8f7d16c1c20a926a6e413e54a7
Kirill Mokevnin <mokevnin@gmail.com>
2020-03-25 14:57:40 -0400

    add credo config

---
+++ .credo.exs
@@ -0,0 +1,37 @@
+# config/.credo.exs
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: [
+          "services/web/lib/",
+          "services/web/test/",
+          "services/web/config/"
+        ],
+        excluded: []
+      },
+      # checks: [
+      #   {Credo.Check.Consistency.TabsOrSpaces},
+
+      #   # For some checks, like AliasUsage, you can only customize the priority
+      #   # Priority values are: `low, normal, high, higher`
+      #   {Credo.Check.Design.AliasUsage, priority: :low},
+
+      #   # For others you can also set parameters
+      #   {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 80},
+
+      #   # You can also customize the exit_status of each check.
+      #   # If you don't want TODO comments to cause `mix credo` to fail, just
+      #   # set this value to 0 (zero).
+      #   {Credo.Check.Design.TagTODO, exit_status: 2},
+
+      #   # To deactivate a check:
+      #   # Put `false` as second element:
+      #   {Credo.Check.Design.TagFIXME, false},
+
+        # ... several checks omitted for readability ...
+      # ]
+    }
+  ]
+}
