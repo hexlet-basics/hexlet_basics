caf221769f48ecc54fd27f2654b009eb25621469
plugin73 <laplugin73@gmail.com>
2020-04-23 09:41:43 +0000

    remove tiller

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -127,37 +127,3 @@ resource "kubernetes_cluster_role_binding" "cluster-admin" {
     name = "kirill.m@hexlet.io"
   }
 }
-
-resource "kubernetes_service_account" "tiller" {
-  # depends_on = ["google_container_cluster.hexlet"]
-
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "tiller-cluster-rule" {
-  # depends_on = ["kubernetes_service_account.tiller"]
-
-  metadata {
-    name = "tiller-cluster-rule"
-  }
-
-  role_ref {
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-    api_group = "rbac.authorization.k8s.io"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    namespace = "kube-system"
-    name      = "tiller"
-    api_group = ""
-  }
-
-  provisioner "local-exec" {
-    command = "helm init --service-account tiller"
-  }
-}
