617d83cda950c72f2747aec327610ac3652ed9cd
plugin73 <laplugin73@gmail.com>
2020-04-29 11:42:25 +0000

    change kubernetes nodes to cpu optimized

--- terraform/dok8s.tf
+++ terraform/dok8s.tf
@@ -1,12 +1,12 @@
-resource "digitalocean_kubernetes_cluster" "hexlet_basics" {
+resource "digitalocean_kubernetes_cluster" "hexlet_basics_2" {
   version = "1.16.6-do.2"
 
-  name = "hexlet-basics"
+  name = "hexlet-basics-2"
   region = "fra1"
 
   node_pool {
-    name       = "hexlet-basics-node-pool"
-    size       = "s-2vcpu-4gb"
+    name       = "hexlet-basics-node-pool-2"
+    size       = "c-2"
     node_count = 2
   }
 }
