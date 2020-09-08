b945b671df205a72f5a3400bc1a63a814afb0a7b
plugin73 <laplugin73@gmail.com>
2020-09-08 08:45:06 +0000

    drop old cluster

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -54,11 +54,11 @@ resource "digitalocean_database_db" "postgres" {
 }
 
 resource "digitalocean_database_firewall" "k8s" {
-  depends_on = [digitalocean_kubernetes_cluster.hexlet_basics_2]
+  depends_on = [digitalocean_kubernetes_cluster.hexlet_basics_3]
 
   cluster_id = digitalocean_database_cluster.hexlet_basics.id
   rule {
     type  = "k8s"
-    value = digitalocean_kubernetes_cluster.hexlet_basics_2.id
+    value = digitalocean_kubernetes_cluster.hexlet_basics_3.id
   }
 }
